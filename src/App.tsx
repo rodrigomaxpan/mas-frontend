145ce1b17a7fce287f7077517716516b7e597b76
Rodrigo Pan <rodrigo@Rodrigos-MacBook-Pro.local>
2021-06-11 00:02:58 -0300

    Manual mergin with master

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import {AppProvider} from './hooks';
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <Router>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+      <GlobalStyle />
+    </Router>
+  );
+}
+
+export default App;
